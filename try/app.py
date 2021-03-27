65cc71c8988b56c7013cc2cde8f051d1b6c24ae6
Shruthi Rao <raoshruthi2001@gmail.com>
2021-03-27 09:15:13 +0530

    added soil desc

--- try/app.py
+++ try/app.py
@@ -11,6 +11,19 @@ app = Flask(__name__)
 model_path = "soilClassify.h5"
 SoilNet = load_model(model_path)
 
+alluvialsoil_desc = """Alluvial soils support more than 40% of India's population by providing the most productive agricultural lands. The soil is porous because of its loamy nature. The proportion of potash, phosphoric acid and alkalies are adequate in alluvial soils. 
+Crops: rice, wheat, sugarcane, tobacco, cotton, jute, maize, oilseeds, vegetables and fruits"""
+
+blacksoil_desc = """Black soils when seen in uplands are considered to be present with low fertility while those in the valleys are very fertile. The black colour arises due to the presence of a small proportion of titaniferous magnetite or iron and black constituents of the parent rock. 
+Crops: cotton (best suited), wheat, jowar, linseed, Virginia, tobacco, castor, sunflower, millets"""
+
+claysoil_desc = """Clay soil has a smooth texture because of its small particle size. It feels sticky and rolls like plasticine when wet. The clay soil swells when wetted and shrink when dried, so a certain amount of restructuring can take place in these soils depending on weather conditions. Because of its density, the clay soil retains moisture well. 
+Crops: paddy (best), vegetables, fruits"""
+
+redsoil_desc = """Red soils originate from parent rocks which are crystalline and metamorphic in nature like acid granites, gneisses and quartzites. On the uplands, the red soils are poor, gravelly and porous but in the lower areas, they are rich, deep dark and fertile. The red color is due to the presence of iron oxide. 
+Crops: cotton, wheat, rice, pulses, millets, tobacco, oil seeds, potatoes and fruits"""
+
+
 def predict_model(img_path, model):
     img = load_img(img_path, target_size=(224, 224))
     img = img_to_array(img)
@@ -34,13 +47,13 @@ def predictSoil():
         result = predict_model(uploadedImg_path, SoilNet)
         
         if (result == 0):
-            return render_template("afterUploadSoil.html", soil_type='Alluvial', user_image_path=uploadedImg_path)
+            return render_template("afterUploadSoil.html", soil_type='Alluvial', user_image_path=uploadedImg_path, desc=alluvialsoil_desc)
         elif (result == 1):
-            return render_template("afterUploadSoil.html", soil_type='Black', user_image_path=uploadedImg_path)
+            return render_template("afterUploadSoil.html", soil_type='Black', user_image_path=uploadedImg_path, desc=blacksoil_desc)
         elif result == 2:
-            return render_template("afterUploadSoil.html", soil_type='Clay', user_image_path=uploadedImg_path)
+            return render_template("afterUploadSoil.html", soil_type='Clay', user_image_path=uploadedImg_path, desc=claysoil_desc)
         elif result == 3:
-            return render_template("afterUploadSoil.html", soil_type='Red', user_image_path=uploadedImg_path)
+            return render_template("afterUploadSoil.html", soil_type='Red', user_image_path=uploadedImg_path, desc=redsoil_desc)
         return "An error occurred"
     return render_template("uploadSoil.html")
 
